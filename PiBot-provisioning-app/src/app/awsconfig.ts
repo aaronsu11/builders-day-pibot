9f86f10c332bd4d09723c90dfded4291478687fd
aaronsu11 <aaronsu11@outlook.com>
2022-10-15 00:07:48 +1100

    add provisioning website config

--- PiBot-provisioning-app/src/app/awsconfig.ts
+++ PiBot-provisioning-app/src/app/awsconfig.ts
@@ -1,7 +1,7 @@
 'use strict';
 
-export var region:string = "aws-region";
-export var cognitoIdPoolId:string = "provisioning-id-pool";
-export var cognitoUserPool:string = "provisioning-user-pool";
-export var cognitoClientId:string = "provisioning-app-id";
+export var region:string = "ap-southeast-2";
+export var cognitoIdPoolId:string = "ap-southeast-2:aad352e0-004c-428c-85fb-18a17e5239fa";
+export var cognitoUserPool:string = "ap-southeast-2_VuTSb82Xw";
+export var cognitoClientId:string = "lgblhl06drvqpnu1bi5s0fvq0";
 export var cognitoIoTPolicy:string = "pibot-provisioning-policy";
